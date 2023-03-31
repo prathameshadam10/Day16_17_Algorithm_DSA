638459650546d3eeb1f72f9cb2b41afae5b24435
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-31 18:57:48 +0530

    Update RegexReplaceExample.java

--- src/com/bridgelabz/RegexReplaceExample.java
+++ src/com/bridgelabz/RegexReplaceExample.java
@@ -10,15 +10,15 @@ public class RegexReplaceExample {
         String message = "Hello <<name>>, We have your full name as <<full name>> in our system. Your contact number is 91-xxxxxxxxxx. Please let us know in case of any clarification. Thank you BridgeLabz 01/01/2016.";
 
         // replace <<name>> by first name
-        String firstName = "Sanjana";
+        String firstName = "Prathamesh";
         message = message.replaceAll("<<name>>", firstName);
 
         // replace <<full name>> by user full name
-        String fullName = "Sanjana Malkar";
+        String fullName = "Prathamesh Adam";
         message = message.replaceAll("<<full name>>", fullName);
 
         // Replace mobile number with your contact number
-        String contactNumber = "91-1234567890"; // Change this to your contact number
+        String contactNumber = "91-8983930906"; // Change this to your contact number
         message = message.replaceAll("91-\\d{10}", contactNumber);
 
         // Replace date with current date
